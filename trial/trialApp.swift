b5be15cc265482c78cf3bd582b2ffb715daf0e38
Shriya Raman <shriya.s.raman@gmail.com>
2023-07-28 10:23:26 -0700

    Initial Commit

---
+++ trial/trialApp.swift
@@ -0,0 +1,17 @@
+//
+//  trialApp.swift
+//  trial
+//
+//  Created by Shriya on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct trialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
